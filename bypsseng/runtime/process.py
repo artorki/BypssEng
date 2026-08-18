bb26770d2df94e14fc0c200b884a5d635ac48b83
Aminreza Torki <108486211+artorki@users.noreply.github.com>
2026-08-18 13:14:37 +0330

    Update process.py

--- bypsseng/runtime/process.py
+++ bypsseng/runtime/process.py
@@ -4,12 +4,12 @@ from core.logger import log
 
 class ProcessManager:
     def __init__(self):
-        self.xray_proc = None
+        self.active_proc = None
         self.dnstt_proc = None
         self.xray_lock = asyncio.Lock()
 
     def kill_stale_processes(self):
-        log("Skipping global stale proxy processes cleanup to prevent killing other instances.", "SOL")
+        log("Skipping global stale proxy processes cleanup...", "SOL")
 
     def cleanup_child_processes(self):
         if self.dnstt_proc and self.dnstt_proc.returncode is None:
@@ -20,12 +20,12 @@ class ProcessManager:
             except Exception: pass
             finally: self.dnstt_proc = None
 
-        if self.xray_proc and self.xray_proc.returncode is None:
+        if self.active_proc and self.active_proc.returncode is None:
             try:
-                self.xray_proc.terminate()
-                try: self.xray_proc.wait(timeout=0.5)
-                except subprocess.TimeoutExpired: self.xray_proc.kill()
+                self.active_proc.terminate()
+                try: self.active_proc.wait(timeout=0.5)
+                except subprocess.TimeoutExpired: self.active_proc.kill()
             except Exception as e: log(f"Error during process cleanup: {e}", "WARN")
-            finally: self.xray_proc = None
+            finally: self.active_proc = None
 
 pm = ProcessManager()
